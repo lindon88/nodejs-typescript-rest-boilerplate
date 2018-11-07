e814cb2f414003a083304864a5cdb35d16e7490e
Nemanja Zoric <nemanja.zoric1.nz@gmail.com>
2018-11-07 15:33:05 +0100

    Started work with tests
    Need to fix - Problem with TypeORM

---
+++ test/api.test.ts
@@ -0,0 +1,17 @@
+import Application from "../app/core/application";
+import chai = require("chai");
+import chaiHttp = require("chai-http");
+import "mocha";
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+const app = Application.bootstrap();
+describe("TEST API REQUEST", () => {
+    it("should return response an call", () => {
+        return chai.request(app).get("/api/test")
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+})
