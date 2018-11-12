d02abe6236c0d3d6a0bd70475d7e55f78e8a963f
Nemanja Zoric <nemanja.zoric1.nz@gmail.com>
2018-11-12 11:46:25 +0100

    Example test - animal and pet

--- app/controllers/example.controller.test.ts
+++ app/controllers/example.controller.test.ts
@@ -70,3 +70,119 @@ describe("Returns specific person", () => {
             });
     });
 });
+
+// test creating animals
+describe("Create new animal test", () => {
+    it("should return message", () => {
+        return chai.request(app).post("/example/animal/create")
+            .send({
+                name: "gorilla",
+            })
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test updating animals
+describe("Update existing animal test", () => {
+    it("should return message", () => {
+        return chai.request(app).post("/example/animal/update")
+            .send({
+                id: 1,
+                name: "gorilla",
+            })
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test deleting animal
+describe("Delete existing animal test", () => {
+    it("should return message", () => {
+        return chai.request(app).del("/example/animal/delete/1")
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test reading all animals
+describe("Read all animals", () => {
+    it("should return response data", () => {
+        return chai.request(app).get("/example/animal/all")
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test reading specific animal
+describe("Read specific animal", () => {
+    it("should return response data", () => {
+        return chai.request(app).get("/example/animal/1")
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test creating new pet
+describe("Create new pet test", () => {
+    it("should return message", () => {
+        return chai.request(app).post("/example/pet/create")
+            .send({
+                animal_id: 1,
+                name: "Grodd",
+                age: 1,
+            })
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test updating existing pet
+describe("Update existing pet test", () => {
+    it("should return message", () => {
+        return chai.request(app).post("/example/pet/update")
+            .send({
+                name: "Grodd",
+                age: 2,
+            })
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test deleting existing pet
+describe("Delete existing pet test", () => {
+    it("should return message", () => {
+        return chai.request(app).del("/example/pet/delete/1")
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test reading all pets
+describe("Read all pets test", () => {
+    it("should return response data", () => {
+        return chai.request(app).get("/example/pet/all")
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
+
+// test reading specific pet
+describe("Read specific pet", () => {
+    it("should return response data", () => {
+        return chai.request(app).get("/example/pet/1")
+            .then((res) => {
+                chai.expect(res.body).to.eql({});
+            });
+    });
+});
